432560396b0be6915a8f31cdf00e781107b67a08
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-16 13:59:17 +0530

    Add toLowerCase method of the String

--- src/CWH_14_String_methods.java
+++ src/CWH_14_String_methods.java
@@ -2,13 +2,20 @@ public class CWH_14_String_methods {
     public static void main(String[] args) {
 
         // (1) Length method in Strings.
-        String name = "Sahil is the good boy";
+        String name = "Sahil Is The Good Boy";
         System.out.println(name);
         int value = name.length();
         System.out.println(value);
         // the length method is use to define the length of the string.
         // length method is start form zero to infinite. (ex:- 0,1,2,3,4,5,....)
         // length method is also read the white spaces between the strings.
+        // Output:- Sahil Is The Good Boy
+
+        // (2) toLowerCase method of strings.
+        String lString = name.toLowerCase();
+        System.out.println(lString);
+        // the toLowerCase is use to transform the full string in to the order of lower case.
+        // Output:- sahil is the good boy  [ All the character of string in lower case.]
 
     }
 }
