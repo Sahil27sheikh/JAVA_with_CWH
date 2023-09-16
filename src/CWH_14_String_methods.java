0f6322c8ae6882e68d893fa017a971c004ec2823
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-16 13:49:11 +0530

    new file String methods and add length method of string

---
+++ src/CWH_14_String_methods.java
@@ -0,0 +1,14 @@
+public class CWH_14_String_methods {
+    public static void main(String[] args) {
+
+        // (1) Length method in Strings.
+        String name = "Sahil is the good boy";
+        System.out.println(name);
+        int value = name.length();
+        System.out.println(value);
+        // the length method is use to define the length of the string.
+        // length method is start form zero to infinite. (ex:- 0,1,2,3,4,5,....)
+        // length method is also read the white spaces between the strings.
+
+    }
+}
