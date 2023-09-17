6f25173f50224f0fb9d72937622a17bb5b48cede
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-17 13:51:28 +0530

    Add Escape Sequence Character file Withe double quotes character

---
+++ src/CWH_14_escape_Sequence_Character.java
@@ -0,0 +1,23 @@
+public class CWH_14_escape_Sequence_Character {
+    public static void main(String[] args) {
+
+        // \" double quotes
+        // this method is use to add the double quot on the string.
+        System.out.println("I am \"escape sequence\" character.");
+        // Output: I am "escape sequence" character.
+
+
+
+    }
+}
+
+/*
+\t 	It is used to insert a tab in the text at this point.
+\' 	It is used to insert a single quote character in the text at this point.
+\" 	It is used to insert a double quote character in the text at this point.
+\r 	It is used to insert a carriage return in the text at this point.
+\\ 	It is used to insert a backslash character in the text at this point.
+\n 	It is used to insert a new line in the text at this point.
+\f 	It is used to insert a form feed in the text at this point.
+\b 	It is used to insert a backspace in the text at this point.
+*/
