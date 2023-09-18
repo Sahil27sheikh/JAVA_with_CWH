b5b2d012ebd53cfcf01493ecafe114894d82201e
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-18 17:14:50 +0530

    Add enhance Switch

--- src/CWH_18_Switch_Case.java
+++ src/CWH_18_Switch_Case.java
@@ -20,5 +20,19 @@ public class CWH_18_Switch_Case {
                 System.out.println("enjoy your life");
         }
 
+
+        // Enhance Switch method of switch.
+        // in this method break class is not important.
+        // -> is use after case.
+
+        switch ("cricket") {
+            case "cricket" -> System.out.println("you are going to become a cricketer!");
+            // break is use to break the class. if you are not use break it execute all the cases after then include it.
+            case "football" -> System.out.println("you are going to become a footballer!");
+            case "Hockey" -> System.out.println("you are going to become a hockey champion!");
+            default -> System.out.println("enjoy your life");
+        }
+        // Output: you are going to become a cricketer!
+
     }
 }
