98d201b2202de64f5d3e9670bd2ca1e9d2cbd3fe
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-18 16:04:16 +0530

    Creat new file logical conditionals and add practice of logical AND &&

---
+++ src/CWH_17_logical_conditionals.java
@@ -0,0 +1,22 @@
+public class CWH_17_logical_conditionals {
+    public static void main(String[] args) {
+
+        // Logical Operators:
+        // && is AND (Both are True)
+        System.out.println("For Logical AND");
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+
+        if (a && b && c){
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+        // Output: NO
+
+
+
+    }
+}
