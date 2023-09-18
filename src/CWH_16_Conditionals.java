624809db30f3bc02441c0fd6e637b6440c2500ba
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-18 15:38:32 +0530

    Creat a new file of Conditional and writhe the practice code

---
+++ src/CWH_16_Conditionals.java
@@ -0,0 +1,11 @@
+public class CWH_16_Conditionals {
+    public static void main(String[] args) {
+        int a = 18;
+        if (a>=18){
+            System.out.println("Yes Boy You Can Drive");
+        }
+        else { // else is optional because without else the program is run.
+            System.out.println("No Boy You Can Not Drive Yet");
+        }
+    }
+}
