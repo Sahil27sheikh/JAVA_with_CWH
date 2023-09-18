ad0b7c756ad34750397b77da4f1b787f85316a86
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-18 16:06:34 +0530

    Add logical NOT !

--- src/CWH_17_logical_conditionals.java
+++ src/CWH_17_logical_conditionals.java
@@ -27,7 +27,14 @@ public class CWH_17_logical_conditionals {
         // Output: Yes // it is evaluate in Yes because of any one is true
 
 
+        // ! is NOT
+        // it is print the opposite value.
+        // it is also known as inverter because it is invert the value.
+        System.out.println("Not (a) is: "+!a);
+        // output: Not (a) is: false   // it is print false because of the value of a is true.
 
+        System.out.println("Not (b) is: "+!b);
+        // output: Not (a) is: false   // it is print true because of the value of b is false.
 
     }
 }
