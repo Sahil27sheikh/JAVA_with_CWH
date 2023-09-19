6d143f9fe891a6c5ccabb70becb648244ac137bf
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-19 14:13:26 +0530

    solve question 2

--- src/CWH_19_Ch4_practice_set.java
+++ src/CWH_19_Ch4_practice_set.java
@@ -15,6 +15,28 @@ public class CWH_19_Ch4_practice_set {
         */
 
 
+        // Question 2:
+        // Get the input form user of 3 subjects and show it you are pass or fail.
+        Scanner sc = new Scanner(System.in);
+        byte m1,m2,m3;
+        System.out.print("Enter Your marks in physics: ");
+        m1 = sc.nextByte();
+
+        System.out.print("Enter Your marks in Chemistry: ");
+        m2 = sc.nextByte();
+
+        System.out.print("Enter Your marks in Maths: ");
+        m3 = sc.nextByte();
+
+        float percentage = (m1+m2+m3)/3.0f;
+        System.out.println("Your Percentage is %f: "+percentage);
+
+        if (percentage>=40 && m1>=33 && m2>=33 && m3>=33){
+            System.out.println("Congratulations, you have been promoted");
+        }
+        else {
+            System.out.println("Sorry, You have not been promoted! Better luck next time");
+        }
 
     }
 }
