97e26cfd534f2aaa362c843b4050d77191f5cb8c
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-19 14:12:36 +0530

    Creat new file of chapter 4 practice set and solve 1 question

---
+++ src/CWH_19_Ch4_practice_set.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class CWH_19_Ch4_practice_set {
+    public static void main(String[] args) {
+
+        // Question 1:
+        /*
+        int a = 10; // it is a error because of '=' is the assignment operator and in the if we can not able to
+                    // use assignment operators in the if. (and that condition '==' is use because of it is the conditionals operators.
+        if (a=11){
+            System.out.println("I am 11");
+        }
+        else {
+            System.out.println("I am not 11");
+        }
+        */
+
+
+
+    }
+}
