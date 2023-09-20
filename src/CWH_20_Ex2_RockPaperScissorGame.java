d0208f83b3677b3abf06e05126606ba19b043433
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-20 15:34:50 +0530

    compleat the code and enjoy the game

--- src/CWH_20_Ex2_RockPaperScissorGame.java
+++ src/CWH_20_Ex2_RockPaperScissorGame.java
@@ -10,10 +10,10 @@ public class CWH_20_Ex2_RockPaperScissorGame {
         Random r = new Random();
 
         System.out.println("Let's play Rock, Paper, Scissors!");
-        System.out.println("0 = Rock, 1 = Paper and 2 = Scissor");
+        System.out.println("Rules: \n\tRock = 0, Paper = 1 and Scissor = 2");
 
         // User input
-        System.out.print("Enter your choice (0 = Rock, 1 = Paper, or 2 = Scissors): ");
+        System.out.print("Enter your choice: ");
         int userInput = sc.nextInt();
 
         // Computer input
@@ -29,9 +29,26 @@ public class CWH_20_Ex2_RockPaperScissorGame {
             System.out.println("Congratulation You Won");
         }
 
+        else if (userInput<0 || userInput>2) {
+            System.out.println("You Have Entered Wrong Number");
+        }
+
         else {
             System.out.println("Sorry, You lose! Better Luck Next Time");
         }
+
+        switch (userInput){
+            case 0 -> System.out.println("Your Choice Is: \"Rock\"");
+            case 1 -> System.out.println("Your Choice Is: \"Paper\"");
+            case 2 -> System.out.println("Your Choice Is: \"Scissor\"");
+        }
+
+        switch (comInput){
+            case 0 -> System.out.println("Computer Choice Is: \"Rock\"");
+            case 1 -> System.out.println("Computer Choice Is: \"Paper\"");
+            case 2 -> System.out.println("Computer Choice Is: \"Scissor\"");
+        }
+        System.out.print("Thank You For Playing \nPlay Again...");
 
 
 
