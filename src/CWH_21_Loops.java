c54170e50a00a0e3dfadb600e73aaadaaca8722b
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-20 19:34:31 +0530

    Add Quick Qiez 1

--- src/CWH_21_Loops.java
+++ src/CWH_21_Loops.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
 public class CWH_21_Loops {
     // Their are three types of loops in java
     public static void main(String[] args) {
 
-        System.out.println("Using While Loop");
-        int i = 1;
-        while (i<=5) {
-            System.out.println(i);
-            i++;
-        }
-        System.out.println("Finish while loop");
+//        System.out.println("Using While Loop");
+//        int i = 1;
+//        while (i<=5) {
+//            System.out.println(i);
+//            i++;
+//        }
+//        System.out.println("Finish while loop");
+
+        // Quick Quiz 1
+        // print natural number from 100 to 200
+//        int a = 100;
+//        while (a<=200){
+//            System.out.println(a);
+//            a++;
+//
+//        }
+
     }
 }
