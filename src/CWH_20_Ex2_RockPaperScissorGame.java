db53ca2a4b499431db21e90241daa9c12f6bc8a4
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-20 14:19:03 +0530

    Write the code to get the input the user and Computer

--- src/CWH_20_Ex2_RockPaperScissorGame.java
+++ src/CWH_20_Ex2_RockPaperScissorGame.java
@@ -6,6 +6,19 @@ public class CWH_20_Ex2_RockPaperScissorGame {
 
         // Create the game of Rock, Paper, Scissor
 
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+
+        System.out.println("Let's play Rock, Paper, Scissors!");
+        System.out.println("r = Rock, p = Paper and s = Scissor");
+
+        // User input
+        System.out.print("Enter your choice (Rock, Paper, or Scissors): ");
+        String userInput = sc.nextLine().toLowerCase();
+
+        // Computer input
+        int comInput = r.nextInt(3);
+        String compInput = "";
 
 
     }
