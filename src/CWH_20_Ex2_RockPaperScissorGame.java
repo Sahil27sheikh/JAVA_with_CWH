06152cc5a6143e023b265aaf9fab990cb3979ff7
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-20 15:03:22 +0530

    Apply comditions on programm

--- src/CWH_20_Ex2_RockPaperScissorGame.java
+++ src/CWH_20_Ex2_RockPaperScissorGame.java
@@ -10,15 +10,29 @@ public class CWH_20_Ex2_RockPaperScissorGame {
         Random r = new Random();
 
         System.out.println("Let's play Rock, Paper, Scissors!");
-        System.out.println("r = Rock, p = Paper and s = Scissor");
+        System.out.println("0 = Rock, 1 = Paper and 2 = Scissor");
 
         // User input
-        System.out.print("Enter your choice (Rock, Paper, or Scissors): ");
-        String userInput = sc.nextLine().toLowerCase();
+        System.out.print("Enter your choice (0 = Rock, 1 = Paper, or 2 = Scissors): ");
+        int userInput = sc.nextInt();
 
         // Computer input
         int comInput = r.nextInt(3);
-        String compInput = "";
+
+        // conditions
+        if (userInput == comInput){
+            System.out.println("The Match Is Tie!");
+
+        }
+
+        else if (userInput==0 && comInput==2 || userInput==1 && comInput==0 || userInput==2 && comInput==1) {
+            System.out.println("Congratulation You Won");
+        }
+
+        else {
+            System.out.println("Sorry, You lose! Better Luck Next Time");
+        }
+
 
 
     }
