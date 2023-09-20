c46af59663b4d7959964559d86819de4ff6d9480
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-20 19:12:47 +0530

    Creat the file for loop and add the one example of the whilw loop

---
+++ src/CWH_21_Loops.java
@@ -0,0 +1,13 @@
+public class CWH_21_Loops {
+    // Their are three types of loops in java
+    public static void main(String[] args) {
+
+        System.out.println("Using While Loop");
+        int i = 1;
+        while (i<=5) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish while loop");
+    }
+}
