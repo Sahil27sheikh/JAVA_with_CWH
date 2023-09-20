8342b752940f20fa8f775d1299f5459b6ce302a3
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-20 20:00:26 +0530

    Add examples of do while loop

--- src/CWH_22_doWhileLoop.java
+++ src/CWH_22_doWhileLoop.java
@@ -1,2 +1,23 @@
 public class CWH_22_doWhileLoop {
+    public static void main(String[] args) {
+        // the while loop is first check the programme and then execute.
+
+        // do-while loop
+        // in do while loop first is enter the programme and then check the condition.
+        // it is the guaranteed to execute at least ones.
+        int b = 0;
+        do {
+            System.out.println(b);
+            b++;
+        }
+        while (b<+5);
+
+
+        int c = 10;
+        do {
+            System.out.println(c);
+            b++;
+        }
+        while (c<+5);
+    }
 }
