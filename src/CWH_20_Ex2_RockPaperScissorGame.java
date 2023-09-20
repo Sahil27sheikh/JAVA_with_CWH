c448fc989de91eb5c0eef2a4793602c168788009
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-20 14:18:17 +0530

    Creat new file for the of Rock, Paper and Scissor.

---
+++ src/CWH_20_Ex2_RockPaperScissorGame.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class CWH_20_Ex2_RockPaperScissorGame {
+    public static void main(String[] args) {
+
+        // Create the game of Rock, Paper, Scissor
+
+
+
+    }
+}
