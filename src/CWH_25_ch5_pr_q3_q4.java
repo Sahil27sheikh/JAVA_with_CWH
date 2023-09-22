8c60e6d535d0c850949bff38c2cf29fc7f1e66fd
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-22 18:29:34 +0530

    remane the file and solve the question 5 of pr set. multiplication table in reverse order

--- src/CWH_25_ch5_pr_q3_q4.java
+++ src/CWH_25_ch5_pr_q3_q4.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
-public class CWH_25_ch5_pr_q3 {
+public class CWH_25_ch5_pr_q3_q4 {
     public static void main(String[] args) {
+         // Question 3
         // write a java programme of "n" number of multiplication table.
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
@@ -8,6 +9,16 @@ public class CWH_25_ch5_pr_q3 {
         System.out.println("Your Multiplication Table Of Number "+a);
         for (long i=1; i<=10; i++){
             System.out.printf("%d X %d = %d\n",a, i, a*i);
+        }
+
+
+        // Question 4
+        // write a java programme of "n" number of multiplication table in reverse order.
+        System.out.print("Enter the number: ");
+        long b = sc.nextInt();
+        System.out.println("Your Multiplication Table Of Number "+a);
+        for (long i=10; i>=1; i--) {
+            System.out.printf("%d X %d = %d\n", b, i, b * i);
         }
     }
 }
