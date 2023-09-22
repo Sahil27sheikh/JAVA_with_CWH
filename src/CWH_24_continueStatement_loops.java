9b2a893be0bb0c6b33f9879b9b8921886bb2ece8
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-22 12:03:41 +0530

    use continue statement with for loop

--- src/CWH_24_continueStatement_loops.java
+++ src/CWH_24_continueStatement_loops.java
@@ -1,5 +1,13 @@
 public class CWH_24_continueStatement_loops {
     public static void main(String[] args) {
 
+        // Continue is use withe for loop.
+        for (int i=0; i<50; i++){
+            if (i == 5){
+                System.out.println("Ending the loop 5");
+                continue; // continue is skip the value 5 and continue to forward the loop.
+            }
+            System.out.println("java is great "+i);
+        }
     }
 }
