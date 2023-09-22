76aef932b7688f7f704b30612169b4c2426bc121
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-22 18:04:34 +0530

    Solve question 2 of add first even number of

--- src/CWH_25_ch5_practiceSet_q2.java
+++ src/CWH_25_ch5_practiceSet_q2.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class CWH_25_ch5_practiceSet_q2 {
     public static void main(String[] args) {
+
+        // Write the java programme to sum first "n" even numbers using while loops.
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        System.out.print("Enter the number: ");
+        int number= sc.nextInt();
+        for ( int item=0; item<number; item++ ){
+            sum = sum + (2*item);
+
+        }
+        System.out.print("Sum of the even numbers are is: ");
+        System.out.println(sum);
+        /* even numbers is 0, 2 ,4, 6, 8, 10..... */
 
     }
 }
