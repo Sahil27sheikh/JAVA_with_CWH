d38a49c0da132a6eb60088063cfdce4d60e61582
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-22 19:19:59 +0530

    solve Question 5

---
+++ src/CWH_25_ch5_pr_q5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class CWH_25_ch5_pr_q5 {
+    public static void main(String[] args) {
+        // Question 5
+        // write the java of the find the factorial of the given number.
+
+
+
+        // Question 5
+        // write the java of the find the factorial of the given number.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the number: ");
+        long n = sc.nextInt();
+        long i = 1L;
+        long factorial = 1L; // the value of the factorial is use 1 because of when "0" is the value the multiplication is always "0"
+                             // "0" is use for sum and minus.
+        while (i<=n){
+            factorial *= i;
+            i++;
+        }
+        System.out.printf("The factorial of %d is %d",n,factorial);
+        // Note: this programme is able to print the factorial of the largest number is 65.
+    }
+}
+
+// What is factorial?
+// n = n * n-1 * n-2* n-3 * n-4 *.....
+// means the factorial of the 5 is = 5! = 5*4*3*2*1
+// ! is the symbol of the factorial
