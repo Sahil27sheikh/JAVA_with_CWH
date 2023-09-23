a8ebb047a122eead6550ff26a3f64fb8812d14be
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-23 12:01:47 +0530

    Solve Question 7 Repeat Question 1 With While Loop In New File

---
+++ src/CWH_25_ch5_pr_q7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CWH_25_ch5_pr_q7 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int a =sc.nextInt();
+        int i =a;
+        while(i>0){
+            i--;
+            for (int k=0; k<=i; k++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
