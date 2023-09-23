bf1aa6caaf45c619d12d92c2a218dd31a9e20a18
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-23 12:04:04 +0530

    modify file

--- src/CWH_25_ch5_pr_q7.java
+++ src/CWH_25_ch5_pr_q7.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class CWH_25_ch5_pr_q7 {
+    // Repeat question 1 using while loop.
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.print("Enter the number: ");
