899635e44d3034ea25d6aab1664ba4125c4bade3
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-24 13:10:53 +0530

    write the first programme of Arrays and exicute it

--- src/CWH_26_Arrays.java
+++ src/CWH_26_Arrays.java
@@ -7,6 +7,13 @@ public class CWH_26_Arrays {
         2) Use Arrays (recommended)
         */
         // Basis structure to create new Arrays.
-        int [] marks = new int[5];
+        int [] marks = new int[5]; // marks is the variable name.
+        marks[0] = 100;
+        marks[1] = 10;
+        marks[2] = 56;
+        marks[3] = 99;
+        marks[4] = 30;
+        marks[4] = 20; // We can also able to overwrite the arrays.
+        System.out.println(marks[2]); // to print the Arrays
     }
 }
