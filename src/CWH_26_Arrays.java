49ac9d984259454b15f5c73542231f8cb5f1b3ef
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-24 12:54:58 +0530

    creat new array file

---
+++ src/CWH_26_Arrays.java
@@ -0,0 +1,12 @@
+public class CWH_26_Arrays {
+    public static void main(String[] args) {
+        /*
+        Classroom of 500 students - you have store marks if 500 students
+        You have two options;
+        1) Create 500 variables
+        2) Use Arrays (recommended)
+        */
+        // Basis structure to create new Arrays.
+        int [] marks = new int[5];
+    }
+}
