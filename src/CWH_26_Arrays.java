e0cb812005059fa9888d9b28a8585cbdb4500eb3
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-24 13:34:52 +0530

    Write second and third method of use arrays

--- src/CWH_26_Arrays.java
+++ src/CWH_26_Arrays.java
@@ -7,13 +7,31 @@ public class CWH_26_Arrays {
         2) Use Arrays (recommended)
         */
         // Basis structure to create new Arrays.
-        int [] marks = new int[5]; // marks is the variable name.
-        marks[0] = 100;
+
+        // first type to write arrays.
+        // Declaration and memory allocation.
+        // int [] marks = new int[5];
+
+        // Second type to write arrays.
+        // Declaration and then memory allocation.
+        // int [] marks;
+        // marks = new int[];
+        /*
+        marks[0] = 100; // This is the Array element.
+        // Initialization.
         marks[1] = 10;
         marks[2] = 56;
         marks[3] = 99;
         marks[4] = 30;
         marks[4] = 20; // We can also able to overwrite the arrays.
+        //  marks[5] = 38; // Throws an error
+        */
+
+        // Third type to writhe the arrays
+        // Declaration, memory allocation and initialization together
+        int [] marks = {100, 10, 56, 99, 30};
+
+
         System.out.println(marks[2]); // to print the Arrays
     }
 }
