d5aabbb9851fb8aab9c4dadc9569428f5119e8de
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-25 13:00:14 +0530

    solve question 3- The average marks of the students

--- src/CWH_29_PracticeSet.java
+++ src/CWH_29_PracticeSet.java
@@ -15,7 +15,7 @@ public class CWH_29_PracticeSet {
         // question 2
         // Write a programme to find out weather a Given integer is present in array or not.
         Scanner sc = new Scanner(System.in);
-        System.out.println("Question 1");
+        System.out.println("Question 2");
         float [] b = {22.5f, 10.4f, 34.6f, 50.34f, 45.4f};
         System.out.print("Enter the number: ");
         float num = sc.nextFloat();
@@ -31,6 +31,18 @@ public class CWH_29_PracticeSet {
         }
         else {
             System.out.println("The value are not present in the array");
+
         }
+
+
+        // Question 3
+        // Calculate the average marks of the array containing marks of all students in physics using for each loop.
+        System.out.println("Question 3");
+        float [] c = {22.5f, 10.4f, 34.6f, 50.34f, 45.4f};
+        float total = 0;
+        for (float i:c){
+            total = total+i;
+        }
+        System.out.println("the average marks of the students is : "+total/c.length);
     }
 }
