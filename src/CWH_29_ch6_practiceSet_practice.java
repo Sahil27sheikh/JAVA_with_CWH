7b48d3ee9ef3fea09dcbef79c27b3e3ed542816a
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-29 19:41:45 +0530

    resolve question 2 and 3 of chapter 6 for practice

--- src/CWH_29_ch6_practiceSet_practice.java
+++ src/CWH_29_ch6_practiceSet_practice.java
@@ -12,5 +12,33 @@ public class CWH_29_ch6_practiceSet_practice {
         // Output: This is the sum of the float: 17.039999
 
 
+        // Question 2
+        int [] a = {1,3,5,45,23,534,23,1};
+        System.out.print("Enter the number for check the value: ");
+        int num = sc.nextInt();
+        boolean isInArray = false;
+        for (int i:a){
+            if (num==i){
+                isInArray= true;
+                break;
+            }
+        }
+        if (isInArray){
+            System.out.println("The number is present in the array");
+        }
+        else {
+            System.out.println("The number is not present in the array");
+        }
+
+
+        // Question 3
+        int [] b = {23, 45, 23, 100, 23, 77};
+        int total = 0;
+        for (int i:b){
+            total = total+i;
+        }
+        System.out.println("the average marks of the all students in the physics is: "+total/b.length +"%");
+
+
     }
 }
