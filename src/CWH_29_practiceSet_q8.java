12b94bbd6f90e36bbd61f7d17189ce72b1c7fa7f
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-29 16:24:18 +0530

    add another example of sorted or not question

--- src/CWH_29_practiceSet_q8.java
+++ src/CWH_29_practiceSet_q8.java
@@ -17,5 +17,22 @@ public class CWH_29_practiceSet_q8 {
             System.out.println("The array is not sorted.");
         }
         // Output: The array is sorted.
+
+
+        boolean is = true;
+        int [] a = {1,45, 65, 23, 2};
+        for (int i =0; i< a.length-1; i++){
+            if (a[i] > a[i+1]){
+               is = false;
+               break;
+            }
+        }
+        if (is){
+            System.out.println("The array is sorted.");
+        }
+        else {
+            System.out.println("The array is not sorted.");
+        }
+        // Output: The array is sorted.
     }
 }
