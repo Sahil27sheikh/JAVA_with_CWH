c55f5995f76c23da3b1cced63b0bf4e1ef94a9e9
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-30 16:02:42 +0530

    practice to change the integer and array object with example using static void methods in java

--- src/CWH_32_methods_overloading.java
+++ src/CWH_32_methods_overloading.java
@@ -2,11 +2,41 @@ public class CWH_32_methods_overloading {
     static void tellme(){  // void is use when nothing to return.
         System.out.println("Hii, I am  a sahil form umred city");
     }
+
+    static  void change(int a){
+        a = 45;
+    }
+
+    static  void change2(int []arr){
+        arr[2] = 45;
+    }
     public static void main(String[] args) {
+
+        // Example 1
         tellme();
         // output: Hii, I am  a sahil form umred city
 
         System.out.print("Who are you? ");tellme();
         // Output: Who are you? Hii, I am  a sahil form umred city
+
+        // Example 2
+        // Case 1: Changing the integer.
+        int x = 68;
+        change(x);
+        System.out.println("The value of x after running change is: "+x);
+        // The value can not be change it is pass the copy of the integer.
+
+        // Case 2: Changing the Array.
+        int [] c = {1,2,4,8,9};
+        change2(c);
+        System.out.println("the value of the array after use the change2 method is: "+c[2]);
+        // The value is change because of the array and the objects is change.
+
+        for (int element:c){
+            System.out.print(element +" ");
+            // Output: 1 2 45 8 9
+        }
+
+
     }
 }
