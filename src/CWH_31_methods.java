16ac377ed65812c23c1d1d1b99cdbf586b85aea5
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-30 14:47:55 +0530

    solve another example using resent method

--- src/CWH_31_methods.java
+++ src/CWH_31_methods.java
@@ -11,10 +11,18 @@ public class CWH_31_methods {
     }
     public static void main(String[] args) {
 
+        // Example 1
         int a = 5;
         int b = 7;
         int c;
         c = logic (a,b);  // call the method and interchange the value.
+        System.out.println(c);
+
+        // Example 2
+        int p = 34343;
+        int q = 54543;
+        int r;
+        c = logic(p,q);
         System.out.println(c);
 
     }
