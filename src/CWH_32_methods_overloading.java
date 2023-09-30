f857b1ca0d0c2b76290339b5b6ac20540adc00ed
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-09-30 16:17:08 +0530

    use same name method overloading with different use

--- src/CWH_32_methods_overloading.java
+++ src/CWH_32_methods_overloading.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class CWH_32_methods_overloading {
     static void tellme(){  // void is use when nothing to return.
         System.out.println("Hii, I am  a sahil form umred city");
@@ -10,6 +11,18 @@ public class CWH_32_methods_overloading {
     static  void change2(int []arr){
         arr[2] = 45;
     }
+
+
+    // Method overloading.
+    static void bro(){
+        System.out.println("Good morning bro!");
+    }
+
+    static void bro(int m){   // Same name of method but different uses. and because of this reason java can not throw error.
+        System.out.println("Good morning "+m+" times bro!");
+    }
+
+
     public static void main(String[] args) {
 
         // Example 1
@@ -36,6 +49,15 @@ public class CWH_32_methods_overloading {
             System.out.print(element +" ");
             // Output: 1 2 45 8 9
         }
+        System.out.println();
+
+
+        // Method overloading
+        bro();
+        // Output: Good morning bro!
+
+        bro(5000);
+        // Output: Good morning 5000 times bro!
 
 
     }
