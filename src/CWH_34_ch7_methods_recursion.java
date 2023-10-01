401d3618c28ee1286762a4be2ce8d4a97dc725b4
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-10-01 13:08:32 +0530

    solve above example using for loop

--- src/CWH_34_ch7_methods_recursion.java
+++ src/CWH_34_ch7_methods_recursion.java
@@ -13,10 +13,29 @@ public class CWH_34_ch7_methods_recursion {
                 return n * factorial(n-1);  // it is call itself.
             }
     }
+
+    // using for loop
+    static long factorial_iterative(long x){
+            if (x==0 || x==1){
+                return 1;
+            }
+            else {
+                int product = 1;
+                for (long i = 1; i <= x; i++) {
+                    product *= i;
+                }
+                return product;
+            }
+    }
     public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
             long a = sc.nextInt();
-        System.out.printf("The value of factorial %d is %d",a, factorial(a));
+        System.out.printf("The value of factorial %d using factorial method is %d",a, factorial(a));
+        System.out.println();
+
+        System.out.print("Enter the number: ");
+        long b = sc.nextInt();
+        System.out.printf("The value of factorial %d using factorial_iterative method is %d",a, factorial_iterative(b));
     }
 }
