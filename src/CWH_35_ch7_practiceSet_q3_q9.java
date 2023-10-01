b8d632d2bd4a4a4b215a83e00fca22ffad50066b
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-10-01 20:23:24 +0530

    solve question 9 in question 3 file using rename. resolve question 3 with iterative approach function

--- src/CWH_35_ch7_practiceSet_q3_q9.java
+++ src/CWH_35_ch7_practiceSet_q3_q9.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class CWH_35_ch7_practiceSet_q3 {
+public class CWH_35_ch7_practiceSet_q3_q9 {
     // Question 3:
     // sum (n) = sum(n-1) + n
     // Example:
@@ -15,6 +15,17 @@ public class CWH_35_ch7_practiceSet_q3 {
             return n + sumNatural(n - 1);
         }
     }
+
+    static int sumNatural2(int n){
+        if (n<=0){
+            return 0;
+        }
+        int sum =0;
+        for (int i=0; i<n; i++){
+            sum += i;
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         // Question 3:
         // Write a recursive function to calculate the sum of first n natural numbers
@@ -22,5 +33,13 @@ public class CWH_35_ch7_practiceSet_q3 {
         System.out.print("Enter the number: ");
         int a = sumNatural(sc.nextInt());
         System.out.println(a);
-    }
+
+
+    // Question 9:
+    // Write a function to calculate the sum of first n natural numbers using iterative approach.
+        System.out.print("Enter the number: ");
+    int b = sumNatural2(sc.nextInt());
+        System.out.println(b);
 }
+}
+
