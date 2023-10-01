ff8302e65b74ce3202780aa180db443e9a99bd05
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-10-01 16:59:31 +0530

    modify code

--- src/CWH_35_ch7_practiceSet_q5.java
+++ src/CWH_35_ch7_practiceSet_q5.java
@@ -1,9 +1,13 @@
 public class CWH_35_ch7_practiceSet_q5 {
     static int fib(int n){
-        if (n==1){
+       /* if (n==1){
             return 0;
         } else if (n==2) {
             return 1;
+        }
+         */
+        if (n==1 || n==2){  // another type with same function.
+            return n-1;
         }
         else {
             return fib(n-1) + fib(n-2);
