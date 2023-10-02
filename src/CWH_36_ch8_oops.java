344fa554a6a43d586c9bb62a789b420cdb597498
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-10-02 20:01:12 +0530

    Add some information about oops in comment

--- src/CWH_36_ch8_oops.java
+++ src/CWH_36_ch8_oops.java
@@ -1,5 +1,36 @@
 public class CWH_36_ch8_oops {
     public static void main(String[] args) {
-
     }
 }
+
+// object oriented programming map the code to real world.
+// it is make the code short and easy to understand.
+
+// What is oops?
+// Solving the problems by creating object is one of the most popular approach in programming
+// is called Object Oriented Programming.
+
+// What is DRY?
+// DRY means Do Not Repeat Yourself.
+// DRY is use to reduce the copy pest of the code and reduce the mentanance and size of code.
+
+// Class:
+// A class is a blueprint for creation Object.
+// Classes is use to create the valid Objects.
+// hence class is create the objects.
+// The object and class is related to each other.
+// the one class is create so many objects.
+
+// Object:
+// object is the real world entity created by the class.
+// memory is allocated only after object is created or instantiation.
+
+// How to model a problem in oops?
+// We can find the Noun, Adjective and verbs in out problems.
+// Example to know this-
+// Noun -------------> Class -------> Employee
+// Adjective --------> Attribute ---> name, age, salary of employee
+// verb (Action) ----> Method ------> get salary (), increment ()
+
+
+
