0e4607f0dd803f688200e8db3f5f603951c06c9a
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-10-03 10:28:22 +0530

    Add the information about OOPs terminology and their types in the form of comments

--- src/CWH_36_37_ch8_oops.java
+++ src/CWH_36_37_ch8_oops.java
@@ -32,5 +32,35 @@ public class CWH_36_37_ch8_oops {
 // Adjective --------> Attribute ---> name, age, salary of employee
 // verb (Action) ----> Method ------> get salary (), increment ()
 
+// OOPs terminology (most important):
+// 1) Abstraction (hide internal details)
+// Show only essential details
+// Example:
+// we can buy the phone. we see the look of the phone, display, buttons etc..
+// but we can not se that how its phone features work how to create their features, how to create the phone on company etc..
+// the most relative example of my thinking is the frontend part of the any website or web application is able to see form us but the
+// backend part of this we can not able to see how its created how its work etc...
+
+// 2) Encapsulation
+// encapsulation means so many information or anything is store under a one box.
+// Example:
+// The so many vitamins and dead bacterias are combine together and make the one capsule and tablet.
+// Example 2:
+// laptop is the single entity with WiFy, speakers, storage, etc.. in a single box.
+// with the use of encapsulation the software development in java is easy to use.
+
+// 3) Inheritance
+// The act of deriving new things form existing things.
+// mens the use of old material we can create a new material.
+// Example:
+// Rickshaw to E-Rickshaw
+// phone to smartphone
+// The inheritance is implement the DRY! principle.
+
+// Polymorphism (one entity many forms)
+// Example:
+// smartphone ---> phone,    smartphone ----> calculator,   smartphone ----> camera etc..
+
+
 
 
