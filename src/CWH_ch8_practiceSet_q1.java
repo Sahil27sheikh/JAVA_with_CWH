03b5de24338d06a1a594c0d06577735e43948857
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-10-04 14:58:29 +0530

    solve Question 1

--- src/CWH_ch8_practiceSet_q1.java
+++ src/CWH_ch8_practiceSet_q1.java
@@ -1,7 +1,29 @@
+class Employee1{
+    // Properties:
+    int salary;
+    String name;
 
+    // Methods:
+    public int getSalary(){
+        return salary;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+}
 public class CWH_ch8_practiceSet_q1 {
     public static void main(String[] args) {
         // Question 1
+        Employee1 sahil = new Employee1();
+        sahil.setName("Sahil");
+        sahil.salary = 55000;
+        System.out.println("My Name Is: "+ sahil.getName());
+        System.out.println("My Salary Is: "+ sahil.getSalary());
 
     }
 }
