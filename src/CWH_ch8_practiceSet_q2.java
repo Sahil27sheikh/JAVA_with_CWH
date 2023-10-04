37729ca12dfbc4a40d136ca9402ca922a3975e79
Sahil Sheikh <sahilajajsheikh736@gmail.com>
2023-10-04 15:13:24 +0530

    Solve question 2 Create a class cellphone with methods

---
+++ src/CWH_ch8_practiceSet_q2.java
@@ -0,0 +1,25 @@
+// Question 2:
+// Create a class cellphone with methods to print "ringing...", "vibrating..." etc
+
+class cellphone{
+    public void ring(){
+        System.out.println("Ringing...");
+    }
+
+    public void vibrate(){
+        System.out.println("Vibrating...");
+    }
+
+    public void call(){
+        System.out.println("Calling...");
+    }
+}
+public class CWH_ch8_practiceSet_q2 {
+    public static void main(String[] args){
+        cellphone s1pro = new cellphone();
+        s1pro.call();
+        s1pro.vibrate();
+        s1pro.ring();
+
+    }
+}
